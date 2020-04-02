cfc6ce1e0299ece2284ecfe404e170fed1b4218a
Govjo <jogovaert@gmail.com>
2020-04-02 08:47:14 +0200

    add application, userrepo, ...

---
+++ domain/src/main/java/user/UserRepository.java
@@ -0,0 +1,30 @@
+package user;
+
+import org.springframework.stereotype.Repository;
+import user.exception.EmailAlreadyRegisteredException;
+
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class UserRepository {
+    private final ConcurrentHashMap<String, User> userRepository;
+
+    public UserRepository() {
+        this.userRepository = new ConcurrentHashMap<>();
+    }
+
+    public boolean isEmailAvailable(String email) {
+        if (userRepository.values().stream()
+                .anyMatch(member -> member.getEmail().equals(email))) {
+            throw new EmailAlreadyRegisteredException(email);
+        }
+        return true;
+    }
+
+
+    public Collection<? extends User> getAllUsers() {
+        return userRepository.values();
+    }
+}
